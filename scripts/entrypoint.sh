c7bb907c3b88947b53c48f2646445fbe0336de50
vivekashok1221 <vivekashok1221@gmail.com>
2023-10-28 00:17:32 +0400

    feat: Disable uvicorn auto-reload in production

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+extra_options=""
+if [[ $DEBUG != "false" ]]; then
+    extra_options="--reload --reload-dir ./src"
+fi
+
 poetry run prisma migrate deploy
-poetry run uvicorn src.backend.app:app --host 0.0.0.0 --reload --reload-dir ./src
+poetry run uvicorn src.backend.app:app --host 0.0.0.0 $extra_options
